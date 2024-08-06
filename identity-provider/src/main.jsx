1c2c5a43c4f074a68318ebc7810fd1816838afd3
bmadridgit <benjaminmadrid@computingforall.org>
2024-08-06 15:05:11 -0700

    finished landing page

--- identity-provider/src/main.jsx
+++ identity-provider/src/main.jsx
@@ -1,16 +1,22 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import './main.css'
-
-import LandingPage from './pages/Landing.jsx'
+import './main.css';
+import LandingPage from './pages/LandingPage.jsx';
+import CreateAccount from './pages/CreateAccount.jsx';
+import Login from './pages/Login.jsx';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <LandingPage />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/createAccount" element={<CreateAccount />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
-)
+);
