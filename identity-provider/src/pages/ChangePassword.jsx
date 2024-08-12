875fa88e0a526104b30d8521464a9a6b1c341f21
farheen <farheen.05@outlook.com>
2024-08-12 13:26:12 -0700

    finished change password site

--- identity-provider/src/pages/ChangePassword.jsx
+++ identity-provider/src/pages/ChangePassword.jsx
@@ -6,44 +6,44 @@ import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
 import TextField from '@mui/material/TextField';
 import LockIcon from '@mui/icons-material/Lock';
+import Button from '@mui/material/Button';
 
+import './ChangePassword.css'
 export default function ChangePassword() {
     return (
         <>
-            <h1>Change Your Password</h1>
-            <h5>Your password should be at least 8 characters long and should include a combination of numbers, letters, and special characters (!@#$)</h5>
+            <div class='password-box'>
+                <h1 class="text">Change Your Password</h1>
+                <p class="text">Your password should be at least 8 characters long and should include a combination of numbers, letters, and special characters (!@#$)</p>
 
-            <Box sx={{ '& > :not(style)': { m: 1 } }}>
-                <FormControl variant="standard">
-                    <InputLabel htmlFor="input-with-icon-adornment">
-                        Current Password
-                    </InputLabel>
-                    <Input
-                        id="input-with-icon-adornment"
-                        startAdornment={
-                            <InputAdornment position="start">
-                                <LockIcon />
-                            </InputAdornment>
-                        }
-                    />
-                </FormControl>
-                <TextField
-                    id="input-with-icon-textfield"
-                    label="TextField"
-                    InputProps={{
-                        startAdornment: (
-                            <InputAdornment position="start">
-                                <LockIcon />
-                            </InputAdornment>
-                        ),
-                    }}
-                    variant="filled"
-                />
-                <Box sx={{ display: 'flex', alignItems: 'flex-end' }}>
-                    <LockIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
-                    <TextField id="input-with-sx" label="With sx" variant="outlined"  />
+                <Box sx={{ '& > :not(style)': { m: 1 } }}>
+
+                    <h3 class="text">Current Password</h3>
+                  
+                        <Box class= "passwords" sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                            <LockIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                            <TextField id="current-password" label="Current Password" variant="outlined" />
+                        </Box>
+                 
+                    <h3 id="new-pass">New Password</h3>
+
+                    <Box class= "passwords" sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                        <LockIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                        <TextField id="new-password" label="New Password" variant="outlined" />
+                    </Box>
+
+                    <h3 class="text">Confirm Password</h3>
+
+                    <Box class= "passwords" sx={{ display: 'flex', alignItems: 'flex-end' }}>
+                        <LockIcon sx={{ color: 'action.active', mr: 1, my: 0.5 }} />
+                        <TextField id="confirm-password" label="Confirm Password" variant="outlined" />
+                    </Box>
+
                 </Box>
-            </Box>
+                <Button id = "change-btn" variant="contained" color = "secondary" disableElevation>
+                   Change Password
+                </Button>
+            </div>
         </>
     );
 }
