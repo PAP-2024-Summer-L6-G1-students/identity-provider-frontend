127bca8621779e63d6dd6b6f0063a64afac8ab83
Samantha <sambamtreacy@gmail.com>
2024-08-13 16:21:33 -0700

    finished basic jsx, needs styling and navigation

--- identity-provider/src/main.jsx
+++ identity-provider/src/main.jsx
@@ -13,6 +13,7 @@ import Login from './pages/Login.jsx';
 import EnterCodeFromEmail from './pages/EnterCodeFromEmail.jsx';
 import Dashboard from './pages/Dashboard.jsx';
 import ChangePassword from './pages/ChangePassword.jsx';
+import CreateProfilePage from './pages/CreateProfilePage.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -25,6 +26,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
         <Route path="/create-account" element={<CreateAccount />} />
         <Route path="/forgot-password" element={<ForgotPassword />} />
         <Route path="/login" element={<Login />} />
+       <Route path = "/createprofile" element={<CreateProfilePage />} /> 
       </Routes>
     </Router>
   </React.StrictMode>,
