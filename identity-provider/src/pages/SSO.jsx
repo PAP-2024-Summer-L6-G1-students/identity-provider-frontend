d7312102a43bf2681a8c194836531bd4f530d3b3
Abdiwahid Bishar Hajir <abdiwahidhajir91@gmail.com>
2024-08-14 17:46:23 -0700

    Fixed styling and naming

--- identity-provider/src/pages/SSO.jsx
+++ identity-provider/src/pages/SSO.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import "./SSOPage.css"
+import "./SSO.css"
 function SSOPage() {
   const [apiInfo, setApiInfo] = useState({
     afterLoginRedirectRoute: '',
